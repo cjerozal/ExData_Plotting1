0cbbc3125a1b10375b66b02c58d521247876c644
Cheryl <cjerozal@atlassian.com>
2014-07-12 20:35:42 -0700

    adding plot 3 and its code

--- plot1.R
+++ plot1.R
@@ -28,10 +28,38 @@ fileLocation <- "/home/cheryl/coursera/jh-data-science/working_directory/4-cp1/h
 # dev.off()
 
 
-png(file = "plot2.png")
+# png(file = "plot2.png")
+# library(datasets)
+# 
+# # add a column to the data for POSIXct datetimes
+# currentPowerData$datetime <- apply(currentPowerData, 1, function(row) {
+#     as.POSIXct(
+#         strptime(
+#             paste(row["Date"], row["Time"]),
+#             "%d/%m/%Y %H:%M:%S",
+#             tz = "GMT"),
+#         tz = "GMT")
+# })
+# 
+# with(currentPowerData,
+#      plot(datetime, Global_active_power,
+#           xaxt = "n", # remove default tick marks from x axis
+#           xlab = "",  # remove x axis title
+#           ylab = "Global Active Power (kilowatts)",
+#           type = "l"))
+# 
+# # GMT epoch times for Feb 1 2007, Feb 2 2007, and Feb 3 2007 (midnight)
+# labelNums <- c(1170288000, 1170374370, 1170460740)
+# 
+# axis(side = 1, at = labelNums, labels = c("Thu", "Fri", "Sat"))
+# 
+# dev.off()
+
+
+png(file = "plot3.png")
 library(datasets)
 
-# add a column for actual Date objects
+# add a column to the data for POSIXct datetimes
 currentPowerData$datetime <- apply(currentPowerData, 1, function(row) {
     as.POSIXct(
         strptime(
@@ -41,16 +69,30 @@ currentPowerData$datetime <- apply(currentPowerData, 1, function(row) {
         tz = "GMT")
 })
 
+# create chart to start from
 with(currentPowerData,
-     plot(datetime, Global_active_power,
+     plot(datetime, Sub_metering_1,
+          type = "n",
           xaxt = "n", # remove default tick marks from x axis
           xlab = "",  # remove x axis title
-          ylab = "Global Active Power (kilowatts)",
-          type = "l"))
+          ylab = "Energy sub metering"))
 
 # GMT epoch times for Feb 1 2007, Feb 2 2007, and Feb 3 2007 (midnight)
 labelNums <- c(1170288000, 1170374370, 1170460740)
-
+# add day of week labels to x axis
 axis(side = 1, at = labelNums, labels = c("Thu", "Fri", "Sat"))
+
+# add data for each type of submetering
+with(currentPowerData,
+     points(datetime, Sub_metering_1, type = "l"))
+with(currentPowerData,
+     points(datetime, Sub_metering_2, col = "red", type = "l"))
+with(currentPowerData,
+     points(datetime, Sub_metering_3, col = "blue", type = "l"))
+
+# add legend
+legend("topright", lty = "solid",
+       col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 dev.off()
