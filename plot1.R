a9f1efcb1b0bbaadcdc5b150abe676cb5b0d095c
Cheryl <cjerozal@atlassian.com>
2014-07-12 20:10:40 -0700

    adding plot 2 and its code (in plot 1 file until cleanup at end)

--- plot1.R
+++ plot1.R
@@ -1,30 +1,56 @@
 # Creating a plot to match the provided Plot 1.
 
 # TODO remove
-# fileLocation <- "/home/cheryl/coursera/jh-data-science/working_directory/4-cp1/household_power_consumption.txt"
+fileLocation <- "/home/cheryl/coursera/jh-data-science/working_directory/4-cp1/household_power_consumption.txt"
 
 # TODO fill in
 # download the data
 # then read it in
 
 # TODO bring back
-# powerData <- read.csv(fileLocation, sep = ";", header = TRUE)
+# powerData <- read.csv(fileLocation, sep = ";", header = TRUE,
+#                       colClasses = c("character", "character", rep("numeric", 7)),
+#                       na.strings = c("?"))
+# # We only want to plot the data from 2007-02-01 and 2007-02-02
+# currentPowerData <- powerData[powerData$Date == "1/2/2007" |
+#                               powerData$Date == "2/2/2007", ]
 
-# helper function from
-# http://stackoverflow.com/questions/3418128/how-to-convert-a-factor-to-an-integer-numeric-without-a-loss-of-information
-as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
 
-# We only want to plot the data from 2007-02-01 and 2007-02-02
-currentPowerData <- powerData[powerData$Date == "1/2/2007" |
-                              powerData$Date == "2/2/2007", ]
+# png(file = "plot1.png")
+# library(datasets)
+# 
+# hist(currentPowerData$Global_active_power,
+#      col = "red",
+#      xlab = "Global Active Power (kilowatts)",
+#      main = "Global Active Power",
+#      breaks = 12)
+# 
+# dev.off()
 
-png(file = "plot1.png")
+
+png(file = "plot2.png")
 library(datasets)
 
-hist(as.numeric.factor(currentPowerData$Global_active_power),
-     col = "red",
-     xlab = "Global Active Power (kilowatts)",
-     main = "Global Active Power",
-     breaks = 12)
+# add a column for actual Date objects
+currentPowerData$datetime <- apply(currentPowerData, 1, function(row) {
+    as.POSIXct(
+        strptime(
+            paste(row["Date"], row["Time"]),
+            "%d/%m/%Y %H:%M:%S",
+            tz = "GMT"),
+        tz = "GMT")
+})
+
+with(currentPowerData,
+     plot(datetime, Global_active_power,
+          xaxt = "n", # remove default tick marks from x axis
+          xlab = "",  # remove x axis title
+          ylab = "Global Active Power (kilowatts)",
+          type = "l"))
+
+# GMT epoch times for Feb 1 2007, Feb 2 2007, and Feb 3 2007 (midnight)
+labelNums <- c(1170288000, 1170374370, 1170460740)
+
+axis(side = 1, at = labelNums, labels = c("Thu", "Fri", "Sat"))
 
 dev.off()
